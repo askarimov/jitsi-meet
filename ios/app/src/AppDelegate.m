8bfd12d98f8119b903b2dee1b270226af1f241d7
Askar <askar.k@bilimmail.kz>
2020-09-08 14:27:14 +0600

    Updated appdelegate

--- ios/app/src/AppDelegate.m
+++ ios/app/src/AppDelegate.m
@@ -37,7 +37,10 @@
 
     jitsiMeet.defaultConferenceOptions = [JitsiMeetConferenceOptions fromBuilder:^(JitsiMeetConferenceOptionsBuilder *builder) {
         [builder setFeatureFlag:@"resolution" withValue:@(360)];
-        builder.serverURL = [NSURL URLWithString:@"https://meet.jit.si"];
+//      https://vb1-kt-kzo.bilimland.kz:8443/125ab28b-d223-412b-bfc8-b99d7a8dee31?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb250ZXh0Ijp7InVzZXIiOnsiYXZhdGFyIjoiaHR0cHM6XC9cL3Jtcy5iaWxpbWxhbmQua3pcL3VwbG9hZFwvcHJvZmlsZS02MTcyNDEyXC8xNTk4OTYzNTY0LmpwZWciLCJuYW1lIjoiU21hcnRuYXRpb24gXHUwNDE3XHUwNDMwXHUwNDMyXHUwNDQzXHUwNDQ3IiwiaWQiOjYxNzI0MTJ9fSwiYXVkIjoiaml0c2kiLCJpc3MiOiJvbmxpbmVtZWt0ZXBfaml0c2lfYXBwX2lkIiwic3ViIjoiaHR0cHM6XC9cL29ubGluZW1la3RlcC5vcmciLCJyb29tIjoiMTI1YWIyOGItZDIyMy00MTJiLWJmYzgtYjk5ZDdhOGRlZTMxIiwibW9kZXJhdG9yIjp0cnVlfQ.sn27dBLIlRQmUv_SJtphUgJolW1LSuqnVvHoj5_8Rbk
+        builder.serverURL = [NSURL URLWithString:@"vb1-kt-kzo.bilimland.kz:8443"];
+      builder.room = @"125ab28b-d223-412b-bfc8-b99d7a8dee31";
+      builder.token = @"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb250ZXh0Ijp7InVzZXIiOnsiYXZhdGFyIjoiaHR0cHM6XC9cL3Jtcy5iaWxpbWxhbmQua3pcL3VwbG9hZFwvcHJvZmlsZS02MTcyNDEyXC8xNTk4OTYzNTY0LmpwZWciLCJuYW1lIjoiU21hcnRuYXRpb24gXHUwNDE3XHUwNDMwXHUwNDMyXHUwNDQzXHUwNDQ3IiwiaWQiOjYxNzI0MTJ9fSwiYXVkIjoiaml0c2kiLCJpc3MiOiJvbmxpbmVtZWt0ZXBfaml0c2lfYXBwX2lkIiwic3ViIjoiaHR0cHM6XC9cL29ubGluZW1la3RlcC5vcmciLCJyb29tIjoiMTI1YWIyOGItZDIyMy00MTJiLWJmYzgtYjk5ZDdhOGRlZTMxIiwibW9kZXJhdG9yIjp0cnVlfQ.sn27dBLIlRQmUv_SJtphUgJolW1LSuqnVvHoj5_8Rbk";
         builder.welcomePageEnabled = YES;
       builder.currentLocale = @"kk";
         // Apple rejected our app because they claim requiring a
